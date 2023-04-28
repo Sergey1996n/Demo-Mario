ad08b7bf32be32996854e2e30608e0bdd29e1330
Tsatsenko Sergey <Sergey1996n71@gmail.com>
2023-04-28 13:27:20 +0300

    Add Lesson 10

---
+++ Packages/com.tsatsenko.mario/Runtime/PlayMode/Lesson 10.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Lesson10
+{
+    [Test]
+    public void CheckPlayMode()
+    {
+        Assert.Pass("PlayMode complete!");
+    }
+}
